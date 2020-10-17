58791ee5b89d8eb1c4814bbf30885c92cffeea9f
TadeuA <tadeu01399@gmail.com>
2020-10-17 13:37:43 -0300

    WorkShop4

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+
+import Header from './components/Header';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+
+        <Screen name="OrphanageDetails" component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orphanage" />
+          }} 
+        />
+
+       
+      </Navigator>
+    </NavigationContainer>
+  )
+}
+
+
+
